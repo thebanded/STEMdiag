029626ef41b8957e50e6f8381614ea46ee38b807
Trevor James <ilop7@DESKTOP-VB6MSAO.(none)>
2017-06-10 12:20:13 -0500

    BE: index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 		require 'flight/Flight.php';
-		
-		Flight::route('/asp', function(){
-		    echo 'hello world!';
-		});
 
 		Flight::route('/', function(){
 		    echo 'hello | world!';
+		});
+
+		Flight::route('/asp', function(){
+		    echo 'hello - world!';
 		});
 
 		Flight::start();
